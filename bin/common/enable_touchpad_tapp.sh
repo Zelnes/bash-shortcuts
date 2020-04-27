c53089734922bc250b0ec349dfad8ae1c9a03d70
Mehdi GHESH <mehdi.ghesh@sfr.com>
2020-04-27 15:21:56 +0200

    change the way index is found for the touchpad

--- bin/common/enable_touchpad_tapp.sh
+++ bin/common/enable_touchpad_tapp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-id=$(xinput list | sed -n -r 's/.*TouchPad.*id=([0-9]+).*/\1/p')
+id=$(xinput list | awk -F'[=[:blank:]]+' '/Synaptics/{print $6}')
 prop=$(xinput list-props ${id} | sed -n -r 's/.*Tapping Enabled \(([0-9]+).*/\1/p')
 xinput set-prop ${id} ${prop} 1
